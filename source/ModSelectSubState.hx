be884336fe6f5e09b56fa32dc6c49074d907d8ad
504brandon <mb.bot.official@gmail.com>
2023-02-09 23:34:18 -0600

    first commit

---
+++ source/ModSelectSubState.hx
@@ -0,0 +1,150 @@
+package;
+
+import flixel.input.gamepad.FlxGamepad;
+import openfl.Lib;
+#if windows
+import llua.Lua;
+#end
+import Controls.Control;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.input.keyboard.FlxKey;
+import flixel.system.FlxSound;
+import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+
+class ModSelectSubState extends MusicBeatSubstate
+{
+	var grpMenuShit:FlxTypedGroup<Alphabet>;
+
+	var menuItems:Array<String> = ['Base Game', 'Whitty', 'Smoke Em Out Struggle', '8bit Ryan', 'Sans'];
+	var curSelected:Int = 0;
+
+    public static var inMods:Bool;
+
+	public function new(x:Float, y:Float)
+	{
+		super();
+
+        inMods = true;
+
+		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		bg.alpha = 0;
+		bg.scrollFactor.set();
+		add(bg);
+
+		FlxTween.tween(bg, {alpha: 0.6}, 0.4, {ease: FlxEase.quartInOut});
+
+		grpMenuShit = new FlxTypedGroup<Alphabet>();
+		add(grpMenuShit);
+		
+		for (i in 0...menuItems.length)
+		{
+			var songText:Alphabet = new Alphabet(0, (70 * i) + 30, menuItems[i], true, false);
+			songText.isMenuItem = true;
+			songText.targetY = i;
+            songText.scrollFactor.set();
+			grpMenuShit.add(songText);
+		}
+
+		changeSelection();
+
+		cameras = [FlxG.cameras.list[FlxG.cameras.list.length - 1]];
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		var gamepad:FlxGamepad = FlxG.gamepads.lastActive;
+
+		var upPcontroller:Bool = false;
+		var downPcontroller:Bool = false;
+		var leftPcontroller:Bool = false;
+		var rightPcontroller:Bool = false;
+
+		if (gamepad != null && KeyBinds.gamepad)
+		{
+			upPcontroller = gamepad.justPressed.DPAD_UP;
+			downPcontroller = gamepad.justPressed.DPAD_DOWN;
+			leftPcontroller = gamepad.justPressed.DPAD_LEFT;
+			rightPcontroller = gamepad.justPressed.DPAD_RIGHT;
+		}
+
+		if (controls.UP_P || upPcontroller)
+		{
+			changeSelection(-1);
+   
+		}
+		else if (controls.DOWN_P || downPcontroller)
+		{
+			changeSelection(1);
+		}
+
+		if (controls.ACCEPT)
+		{
+			var daSelected:String = menuItems[curSelected];
+
+            MainMenuState.mod = daSelected;
+
+            FlxG.save.data.curMod = MainMenuState.mod;
+			FlxG.save.flush();
+
+            trace(MainMenuState.mod);
+
+            inMods = false;
+
+            close();
+		}
+
+        if (FlxG.keys.justPressed.ESCAPE){
+            close();
+            inMods = false;
+        }
+
+		if (FlxG.keys.justPressed.J)
+		{
+			// for reference later!
+			// PlayerSettings.player1.controls.replaceBinding(Control.LEFT, Keys, FlxKey.J, null);
+		}
+	}
+
+	override function destroy()
+	{
+		super.destroy();
+	}
+
+	function changeSelection(change:Int = 0):Void
+	{
+		curSelected += change;
+		
+		FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
+
+		if (curSelected < 0)
+			curSelected = menuItems.length - 1;
+		if (curSelected >= menuItems.length)
+			curSelected = 0;
+
+		var bullShit:Int = 0;
+
+		for (item in grpMenuShit.members)
+		{
+			item.targetY = bullShit - curSelected;
+			bullShit++;
+
+			item.alpha = 0.6;
+			// item.setGraphicSize(Std.int(item.width * 0.8));
+
+			if (item.targetY == 0)
+			{
+				item.alpha = 1;
+				// item.setGraphicSize(Std.int(item.width));
+			}
+		}
+	}
+}
